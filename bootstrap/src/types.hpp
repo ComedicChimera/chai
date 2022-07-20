27b463a3e6a7a55a0c930b20dbe8835331f2e645
ComedicChimera <forlornsisu@gmail.com>
2022-07-20 18:22:08 -0400

    implemented all of our types so far

--- bootstrap/src/types.hpp
+++ bootstrap/src/types.hpp
@@ -1,10 +1,15 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
+#include <string>
+#include <vector>
 namespace chai {
     // Enumeration of different kinds of types.
     enum class TypeKind {
-        PRIMITIVE,
+        UNIT,
+        BOOL,
+        INTEGER,
+        FLOATING,
         POINTER,
         FUNCTION,
     };
@@ -13,7 +18,7 @@ namespace chai {
     class Type {
     public:
         // kind returns type's kind.
-        inline virtual TypeKind kind() const = 0;
+        virtual TypeKind kind() const = 0;
 
         // size returns the size of the type in bytes.
         virtual size_t size() const = 0;
@@ -34,7 +39,7 @@ namespace chai {
         // isNullable returns whether the type is nullable.
         inline virtual bool isNullable() const { return true; }
 
-        /* ---------------------------------------------------------------------- */
+        /* ------------------------------------------------------------------ */
 
         // equals returns whether this type is equal the type `other`.
         inline bool equals(Type* other) { 
@@ -58,14 +63,169 @@ namespace chai {
         // outside the implementation of `equals`.
         virtual bool internalEquals(Type* other) const = 0;
 
-        // internalCastFrom returns whether you can cast the type `other` to this
-        // type. This method need only return whether a cast is strictly possible
-        // and doesn't handle inner types.  It should not be called from outside the
+        /* ------------------------------------------------------------------ */
+
+        // internalCastFrom returns whether you can cast the type `other` to
+        // this type. This method need only return whether a cast is strictly
+        // possible (ie. it doesn't need to check if two types are equal) and
+        // doesn't handle inner types.  It should not be called from outside the
         // implementation of `castFrom`.
-        virtual bool internalCastFrom(Type* other) const = 0;
+        inline virtual bool internalCastFrom(Type* other) const { return false; }
     };
 
     /* -------------------------------------------------------------------------- */
+
+    // UnitType represents a unit primitive type.
+    class UnitType : public Type {
+    public:
+        inline TypeKind kind() const override { return TypeKind::UNIT; }
+
+        inline size_t size() const override { return 1; }
+
+        inline std::string repr() const override { return "()"; }
+
+    protected:
+        inline bool internalEquals(Type* other) const override { return other->kind() == TypeKind::UNIT; }
+    };
+
+    // BoolType represents a boolean primitive type.
+    class BoolType : public Type {
+    public:
+        inline TypeKind kind() const override { return TypeKind::BOOL; }
+
+        inline size_t size() const override { return 1; }
+
+        inline std::string repr() const override { return "bool"; }
+
+    protected:
+        inline bool internalEquals(Type* other) const override { return other->kind() == TypeKind::BOOL; }
+
+        inline bool internalCastFrom(Type* other) const override {
+            // int to bool
+            return other->kind() == TypeKind::INTEGER;
+        }
+    };
+
+    // IntegerType represents an integer primitive type.
+    class IntegerType : public Type {
+        // The size of the integer type in bytes.
+        size_t m_size;
+    
+    public:
+        // Whether the integer type is unsigned.
+        bool isUnsigned;
+
+        // Creates a new integer type of the given size and signedness.
+        IntegerType(size_t size, bool isUnsigned)
+        : m_size(size)
+        , isUnsigned(isUnsigned)
+        {}
+
+        inline TypeKind kind() const override { return TypeKind::INTEGER; }
+
+        inline size_t size() const override { return m_size; }
+
+        std::string repr() const override;
+
+    protected:
+        bool internalEquals(Type* other) const override;
+
+        inline bool internalCastFrom(Type* other) const override {
+            // int to bool, int to float
+            return other->kind() == TypeKind::BOOL || other->kind() == TypeKind::FLOATING;
+        }
+    };
+
+    // FloatingType represents a floating-point primitive type.
+    class FloatingType : public Type {
+        // The size of the float type in bytes.
+        size_t m_size;
+
+    public:
+        // Creates a new floating-point type of the given size.
+        FloatingType(size_t size)
+        : m_size(size)
+        {}
+
+        inline TypeKind kind() const override { return TypeKind::FLOATING; }
+
+        inline size_t size() const override { return m_size; }
+
+        std::string repr() const override;
+
+    protected:
+        bool internalEquals(Type* other) const override;
+
+        inline bool internalCastFrom(Type* other) const override {
+            // float to int
+            return other->kind() == TypeKind::INTEGER;
+        }
+    };
+
+    // PointerType represents a pointer type.
+    class PointerType : public Type {
+        // The element type of the pointer.
+        std::unique_ptr<Type> m_elemType;
+
+    public:
+        // The size of a pointer on the desired target.
+        static size_t pointerSize;
+
+        // Whether the pointer is constant.
+        bool isConst;
+
+        // Creates a new pointer type to the `elemType` with `isConst` constancy.
+        PointerType(Type* elemType, bool isConst)
+        : m_elemType(elemType)
+        , isConst(isConst)
+        {}
+
+        // elemType returns a pointer to the element type of the pointer.
+        inline Type* elemType() const { return m_elemType.get(); }
+
+        inline TypeKind kind() const override { return TypeKind::POINTER; }
+
+        inline size_t size() const override { return pointerSize; }
+
+        std::string repr() const override;
+
+    protected:
+        bool internalEquals(Type* other) const override;
+
+        inline bool internalCastFrom(Type* other) const override {
+            // TODO: remove bitcasting later
+            return other->kind() == TypeKind::POINTER;
+        }
+    };
+
+    // FunctionType represents a function type.
+    class FunctionType : public Type {
+        // The return type of the function.
+        std::unique_ptr<Type> m_returnType;
+
+    public:
+        // The parameter types of the function.
+        std::vector<std::unique_ptr<Type>> paramTypes;
+
+        // Creates a new function with the parameter types `paramTypes` and
+        // return type `returnType`.
+        FunctionType(std::vector<std::unique_ptr<Type>>&& paramTypes, Type* returnType)
+        : paramTypes(paramTypes)
+        , m_returnType(returnType)
+        {}
+
+        // returnType returns a pointer to the function's return type.
+        inline Type* returnType() const { return m_returnType.get(); }
+
+        inline TypeKind kind() const override { return TypeKind::FUNCTION; }
+
+        inline size_t size() const override { return PointerType::pointerSize; }
+
+        std::string repr() const override;
+
+    protected:
+        bool internalEquals(Type* other) const override;
+    };
 }
 
 #endif
---
+++ bootstrap/src/types/type_repr.cpp
@@ -0,0 +1,60 @@
+#include "types.hpp"
+
+#include <format>
+
+namespace chai {
+    std::string IntegerType::repr() const {
+        return std::format(
+            "%c%d", 
+            isUnsigned ? 'u' : 'i',
+            m_size
+        );
+    }
+
+    std::string FloatingType::repr() const {
+        return std::format("f%d", m_size);
+    }
+
+    std::string PointerType::repr() const {
+        std::string builder { "*" };
+        
+        if (isConst) {
+            builder += "const ";
+        }
+
+        return builder + m_elemType->repr();
+    }
+
+    std::string FunctionType::repr() const {
+        std::string builder;
+
+        switch (paramTypes.size()) {
+        case 0:
+            builder += "()";
+            break;
+        case 1:
+            builder += paramTypes[0]->repr();
+            break;
+        default:
+            builder += '(';
+
+            int i = 0;
+            for (auto& paramType : paramTypes) {
+                if (i > 0) {
+                    builder += ", ";
+                }
+
+                builder += paramType->repr();
+
+                i++;
+            }
+
+            builder += ')';
+            break;
+        }
+
+        builder += " -> ";
+
+        return builder + m_returnType->repr();
+    }
+}
