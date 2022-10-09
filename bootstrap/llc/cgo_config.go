98dfd780ee901ca95aa06cb42f94d4c908d2af1b
ComedicChimera <forlornsisu@gmail.com>
2022-10-08 23:45:50 -0400

    final update to python version

---
+++ bootstrap/llc/cgo_config.go
@@ -0,0 +1,8 @@
+package llc
+
+/*
+#cgo CFLAGS: -ID:/DevTools/llvm/include -ID:/DevTools/llvm/out/include
+#cgo CXXFLAGS: -std=c++14
+#cgo LDFLAGS: -L D:/DevTools/llvm/out/Release/bin -l:LLVM-C.dll
+*/
+import "C"
