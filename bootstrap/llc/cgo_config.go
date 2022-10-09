2106698eba8934fc09f9a8faa3d7f7317550f0b3
ComedicChimera <forlornsisu@gmail.com>
2022-10-08 23:46:33 -0400

    Revert "final update to python version"
    This reverts commit 98dfd780ee901ca95aa06cb42f94d4c908d2af1b.

--- bootstrap/llc/cgo_config.go
+++
@@ -1,8 +0,0 @@
-package llc
-
-/*
-#cgo CFLAGS: -ID:/DevTools/llvm/include -ID:/DevTools/llvm/out/include
-#cgo CXXFLAGS: -std=c++14
-#cgo LDFLAGS: -L D:/DevTools/llvm/out/Release/bin -l:LLVM-C.dll
-*/
-import "C"
